9bcbca1e71db1c98e2d274ef5531ac1a1180d19b
GeovaniPrestes <geovani@sistemasbr.net>
2025-11-16 01:21:01 -0300

    Criando uma tela para aprender a mecher com eventos

--- BlazorAppTest/BlazorAppTest/Models/Product.cs
+++ BlazorAppTest/BlazorAppTest/Models/Product.cs
@@ -6,6 +6,7 @@
         public string Name { get; set; }
         public double Price { get; set; }
         public bool IsActive { get; set; }
-        public IEnumerable<ProductProp> ProductProp { get; set; }    
+        public IEnumerable<ProductProp> ProductProp { get; set; }
+        public bool ShowProperties { get; set; }
     }
 }
